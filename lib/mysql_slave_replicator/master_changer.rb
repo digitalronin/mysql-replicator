45e01d42248dba1416ceab13c4bf488acd3ba81e
David Salgado <david@digitalronin.com>
2014-04-15 14:59:05 +0100

    allow status to be passed in to change!

--- lib/mysql_slave_replicator/master_changer.rb
+++ lib/mysql_slave_replicator/master_changer.rb
@@ -4,21 +4,19 @@ module MysqlSlaveReplicator
     include Logger
     include MysqlCommand
 
-    attr_accessor :master_host, :mysql_root_password, :file, :position, :replication_user, :replication_password
+    attr_accessor :master_host, :mysql_root_password, :replication_user, :replication_password
 
     def initialize(params)
       @master_host          = params.fetch(:master_host)
       @mysql_root_password  = params.fetch(:mysql_root_password, '')
-      @file                 = params.fetch(:file)
-      @position             = params.fetch(:position)
       @replication_user     = params.fetch(:replication_user)
       @replication_password = params.fetch(:replication_password)
     end
 
-    def change!
+    def change!(status)
       cmds = [
         'stop slave',
-        change_master,
+        change_master(status),
         'start slave'
       ]
       cmd = mysql_command(cmds.join('; '), mysql_root_password)
@@ -28,7 +26,7 @@ module MysqlSlaveReplicator
     private
 
     def change_master
-      %[CHANGE MASTER TO MASTER_LOG_FILE='#{file}', MASTER_LOG_POS=#{position}, MASTER_HOST='#{master_host}', MASTER_USER='#{replication_user}', MASTER_PASSWORD='#{replication_password}']
+      %[CHANGE MASTER TO MASTER_LOG_FILE='#{status[:file]}', MASTER_LOG_POS=#{status[:position]}, MASTER_HOST='#{master_host}', MASTER_USER='#{replication_user}', MASTER_PASSWORD='#{replication_password}']
     end
   end
 end
